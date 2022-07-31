a7b231ec4298edfe515c237591c8d770fb0a7cc8
Suzanne Cabral <25539417+suzannecabral@users.noreply.github.com>
2022-07-31 00:09:54 -0700

    Add dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    // extend plugin:storybook/<configuration>, such as:
+    "plugin:storybook/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
